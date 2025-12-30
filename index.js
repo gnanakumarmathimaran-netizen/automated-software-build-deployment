1a49ac0164795bee489c77715208c61b567a376a
gnanakumarmathimaran <gnanakumar.mathimaran@gmail.com>
2025-12-30 23:21:57 +0530

    Configured React project and ignored node_modules

--- index.js
+++
@@ -1,2 +0,0 @@
-console.log("Automated Software Build and Deployment - DevOps Project Running Successfully- testing");
-console.log("Version 1.0.0");
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Automated Software Build and Deployment</h1>
+      <p>CI/CD Pipeline executed successfully</p>
+      <p>Build & Test completed via GitHub Actions</p>
+    </div>
+  );
+}
+
+export default App;
