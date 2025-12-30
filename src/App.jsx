7e537efde9e8d6df8e86c54639958c13af9d0280
gnanakumarmathimaran <gnanakumar.mathimaran@gmail.com>
2025-12-30 23:39:38 +0530

    change to live - 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="container">
-      <h1>Automated Software Build and Deployment</h1>
+      <h1>Automated Software Build and Deployment new changes</h1>
       <p>CI/CD Pipeline executed successfully</p>
       <p>Build & Test completed via GitHub Actions</p>
     </div>
