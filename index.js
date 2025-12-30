f431a39e0871710a9d72b5f9c32a3bd25272f7fb
gnanakumarmathimaran <gnanakumar.mathimaran@gmail.com>
2025-12-30 23:04:27 +0530

    Demo: trigger CI/CD pipeline

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-console.log("Automated Software Build and Deployment - DevOps Project Running Successfully");
+console.log("Automated Software Build and Deployment - DevOps Project Running Successfully- testing");
 console.log("Version 1.0.0");
