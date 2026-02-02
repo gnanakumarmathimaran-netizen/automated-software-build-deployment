3e715bcc3f39cb59e3fbedf4e7b97840fce7ca6e
gnanakumarmathimaran-netizen <gnanakumar.mathimaran@gmail.com>
2026-02-02 16:07:54 +0530

    issues commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "80px" }}>
       <h1>Automated Software Build and Deployment testing</h1>
       <p>CI/CD Pipeline executed successfully</p>
-      <p>Build &   via Actions</p>
+      <p>Build &   via Actions</p
     </div>
   );
 }
